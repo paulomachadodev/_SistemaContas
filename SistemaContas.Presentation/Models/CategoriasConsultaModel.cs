f51317a55774db754424b0e61b5725602cb1b21a
paulomachadodev <paulomachadodev@gmail.com>
2023-03-02 21:19:04 -0300

    first commit

---
+++ SistemaContas.Presentation/Models/CategoriasConsultaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaContas.Presentation.Models
+{
+    public class CategoriasConsultaModel
+    {
+        /// <summary>
+        /// Modelo de dados para a página de consulta de categorias
+        /// </summary>
+        public Guid IdCategoria { get; set; }
+        public string? Nome { get; set; }
+
+    }
+}
---
+++ SistemaContas.Presentation/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaContas.Presentation.Models
+{
+    public class UsuarioModel
+    {
+        public Guid IdUsuario { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public DateTime DataHoraAcesso { get; set; }
+    }
+}
